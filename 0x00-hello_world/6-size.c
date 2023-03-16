dcf8d670e9c2a4c85161b6981f69743ffdcb30c2
eman <eman.m22omer@gmail.com>
2023-03-16 12:39:14 -0700

    my first c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %u byte(s)\n", sizeof(long long));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+        return (0);
+}
