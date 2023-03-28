5d54fb53556aacab62aa215d0b2abaa8cec59617
eman <eman.m22omer@gmail.com>
2023-03-28 13:46:58 -0700

    me

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Enter main
+ * Description: program that prints _putchar, followed by a new line.
+ * Return: 0
+ */
 int main(void)
 {
-		char *sh = "Holberton";
 
-			while (*sh)
-					{
-								_putchar(*sh);
-										sh++;
-											}
-				_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-					return (0);
+	_putchar('\n');
+	return (0);
 }
